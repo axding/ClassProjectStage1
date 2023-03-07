fcdad4d8897f4a6ba45e0df8b0976ead1248e87c
Amanda Ding <axding@usc.edu>
2023-03-06 19:36:37 -0800

    fixed implementation using fdb

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import com.apple.foundationdb.Database;
 import com.apple.foundationdb.FDB;
+import com.apple.foundationdb.KeyValue;
 import com.apple.foundationdb.Transaction;
 import com.apple.foundationdb.directory.DirectoryLayer;
 import com.apple.foundationdb.directory.DirectorySubspace;
@@ -16,7 +19,6 @@ public class TableManagerImpl implements TableManager{
   private static final FDB fdb;
   private static final Database db;
   private static final DirectorySubspace rootDirectory;
-  private HashMap<String, TableMetadata> tables = new HashMap<>();
 
   static {
     fdb = FDB.selectAPIVersion(710);
@@ -65,9 +67,47 @@ public class TableManagerImpl implements TableManager{
       return StatusCode.TABLE_ALREADY_EXISTS;
     }
 
+    // create table
     DirectorySubspace newTable = rootDirectory.create(db, PathUtil.from(tableName)).join();
-    TableMetadata metadata = new TableMetadata(attributeNames, attributeType, primaryKeyAttributeNames);
-    tables.put(tableName, metadata);
+    Transaction tx = db.createTransaction();
+
+    Tuple attributeKey = new Tuple();
+    attributeKey = attributeKey.add("attributes");
+    Tuple attributeValues = new Tuple();
+
+    for (int i = 0; i < attributeNames.length; i++) {
+      String attributeName = attributeNames[i];
+      AttributeType type = attributeType[i];
+      boolean isPK = false;
+
+      for (String PKAttributeName : primaryKeyAttributeNames) {
+        if (PKAttributeName.equals(attributeName)) {
+          isPK = true;
+        }
+      }
+
+      Tuple keyTuple = new Tuple();
+      keyTuple = keyTuple.add(attributeName);
+
+      Tuple valueTuple = new Tuple();
+      String typeString = "";
+      if (type == AttributeType.INT) {
+        typeString = "INT";
+      }
+      else if (type == AttributeType.VARCHAR) {
+        typeString = "VARCHAR";
+      }
+      else {
+        typeString = "DOUBLE";
+      }
+      valueTuple = valueTuple.add(typeString).add(isPK);
+      tx.set(newTable.pack(keyTuple), valueTuple.pack());
+
+      attributeValues = attributeValues.add(attributeName);
+    }
+    tx.set(newTable.pack(attributeKey), attributeValues.pack());
+    tx.commit().join();
+    tx.close();
 
     return StatusCode.SUCCESS;
   }
@@ -81,7 +121,6 @@ public class TableManagerImpl implements TableManager{
     }
 
     rootDirectory.remove(db, PathUtil.from(tableName)).join();
-    tables.remove(tableName);
 
     return StatusCode.SUCCESS;
   }
@@ -89,7 +128,82 @@ public class TableManagerImpl implements TableManager{
   @Override
   public HashMap<String, TableMetadata> listTables() {
     // your code
-    return tables;
+    HashMap<String, TableMetadata> allTables = new HashMap<>();
+
+    List<String> tableNames = DirectoryLayer.getDefault().list(db, PathUtil.from("root")).join();
+
+    for (String tableName : tableNames) {
+      Transaction tx = db.createTransaction();
+      DirectorySubspace table = rootDirectory.open(db, PathUtil.from(tableName), rootDirectory.getLayer()).join();
+      
+      // obtain list of attribute names
+      Tuple attributes = null;
+      try {
+        Tuple keyTuple = new Tuple();
+        keyTuple = keyTuple.add("attributes");
+        attributes = Tuple.fromBytes(tx.get(table.pack(keyTuple)).get());
+        
+      } catch (Exception e) {
+          System.out.println("ERROR when fetching attribute names: " + e);
+          return null;
+      }
+
+      List<String> attributeNames = new ArrayList<String>();
+      List<AttributeType> attributeTypes = new ArrayList<AttributeType>();
+      List<String> pkAttributes = new ArrayList<String>();
+      
+
+      // find data for each attribute
+      // System.out.println("outside loop");
+      for (int i = 0; i < attributes.size(); i++) {
+        String key = attributes.getString(i);
+        String typeValue = "INT";
+        boolean isPKValue = true;
+        try {
+          // System.out.println(key);
+          Tuple keyTuple = new Tuple();
+          keyTuple = keyTuple.add(key);
+          Tuple values = Tuple.fromBytes(tx.get(table.pack(keyTuple)).get());
+          typeValue = values.getString(0);
+          isPKValue = values.getBoolean(1);
+
+        } catch (Exception e) {
+            System.out.println("ERROR when querying attribute data: " + e);
+            return null;
+        }
+
+        AttributeType type = null;
+        if (typeValue.equals("INT")) {
+          type = AttributeType.INT;
+        } else if (typeValue.equals("VARCHAR")) {
+          type = AttributeType.VARCHAR;
+        }
+        else {
+          type = AttributeType.DOUBLE;
+        }
+
+        // System.out.println(key);
+        // System.out.println(type);
+        // System.out.println(isPKValue);
+
+        attributeNames.add(key);
+        attributeTypes.add(type);
+        if (isPKValue) {
+          pkAttributes.add(key);
+        }
+      }
+
+      // System.out.println(Arrays.toString(attributeNames.toArray()));
+      // System.out.println(Arrays.toString(attributeTypes.toArray()));
+      // System.out.println(Arrays.toString(pkAttributes.toArray()));
+
+      tx.commit().join();
+      tx.close();
+      TableMetadata metadata = new TableMetadata(attributeNames.toArray(new String[0]), attributeTypes.toArray(new AttributeType[0]), pkAttributes.toArray(new String[0]));
+      allTables.put(tableName, metadata);
+    }
+
+    return allTables;
   }
 
   @Override
@@ -104,12 +218,49 @@ public class TableManagerImpl implements TableManager{
       return StatusCode.ATTRIBUTE_TYPE_NOT_SUPPORTED;
     }
 
-    TableMetadata tableMetadata = tables.get(tableName);
-    if (tableMetadata.doesAttributeExist(attributeName)) {
-      return StatusCode.ATTRIBUTE_ALREADY_EXISTS;
+
+    // check if attribute exists
+    Transaction tx = db.createTransaction();
+    DirectorySubspace table = rootDirectory.open(db, PathUtil.from(tableName), rootDirectory.getLayer()).join();
+    Tuple keyTuple = new Tuple();
+    keyTuple = keyTuple.add(attributeName);
+    try {
+      byte[] encodedValues = tx.get(table.pack(keyTuple)).get();
+      if (encodedValues != null) {
+        return StatusCode.ATTRIBUTE_ALREADY_EXISTS;
+      }
+    } catch (Exception e) {
+        System.out.println("ERROR when searching for attribute: " + e);
     }
-    
-    tableMetadata.addAttribute(attributeName, attributeType);
+
+    // add attribute
+    Tuple valueTuple = new Tuple();
+    String typeString = "";
+    if (attributeType == AttributeType.INT) {
+      typeString = "INT";
+    }
+    else if (attributeType == AttributeType.VARCHAR) {
+      typeString = "VARCHAR";
+    }
+    else {
+      typeString = "DOUBLE";
+    }
+    valueTuple = valueTuple.add(typeString).add(false);
+    tx.set(table.pack(keyTuple), valueTuple.pack());
+
+    try {
+      Tuple key = new Tuple();
+      key = key.add("attributes");
+      Tuple attributesTuple = Tuple.fromBytes(tx.get(table.pack(key)).get());
+      
+      attributesTuple = attributesTuple.add(attributeName);
+      tx.set(table.pack(key), attributesTuple.pack());
+    } catch (Exception e) {
+        System.out.println("ERROR when obtain attribute data: " + e);
+    }
+
+    tx.commit().join();
+    tx.close();
 
     return StatusCode.SUCCESS;
   }
@@ -122,14 +273,43 @@ public class TableManagerImpl implements TableManager{
       return StatusCode.TABLE_NOT_FOUND;
     }
 
-    TableMetadata tableMetadata = tables.get(tableName);
-    if (!tableMetadata.doesAttributeExist(attributeName)) {
-      return StatusCode.ATTRIBUTE_NOT_FOUND;
+    // check if attribute exists
+    Transaction tx = db.createTransaction();
+    DirectorySubspace table = rootDirectory.open(db, PathUtil.from(tableName), rootDirectory.getLayer()).join();
+    Tuple keyTuple = new Tuple();
+    keyTuple = keyTuple.add(attributeName);
+
+    try {
+      byte[] encodedValues = tx.get(table.pack(keyTuple)).get();
+      if (encodedValues == null) {
+        return StatusCode.ATTRIBUTE_NOT_FOUND;
+      }
+    } catch (Exception e) {
+        System.out.println("ERROR when searching for attribute: " + e);
     }
 
-    HashMap<String, AttributeType> attributes = tableMetadata.getAttributes();
-    attributes.remove(attributeName);
-    tableMetadata.setAttributes(attributes);
+    // remove attribute
+    tx.clear(table.pack(keyTuple));
+
+    try {
+      Tuple key = new Tuple();
+      key = key.add("attributes");
+      Tuple attributesTuple = Tuple.fromBytes(tx.get(table.pack(key)).get());
+      Tuple newAttributes = new Tuple();
+      
+      for (int i = 0; i < attributesTuple.size(); i++) {
+        String attr = attributesTuple.getString(i);
+        if (!attr.equals(attributeName)) {
+          newAttributes = newAttributes.add(attr);
+        }
+      }
+      tx.set(table.pack(key), newAttributes.pack());
+    } catch (Exception e) {
+        System.out.println("ERROR when obtain attribute data: " + e);
+    }
+
+    tx.commit().join();
+    tx.close();
 
     return StatusCode.SUCCESS;
   }
@@ -141,7 +321,6 @@ public class TableManagerImpl implements TableManager{
     for (String subdirectory : subdirectories) {
       rootDirectory.remove(db, PathUtil.from(subdirectory)).join();
     }
-    tables.clear();
     return StatusCode.SUCCESS;
   }
 }
